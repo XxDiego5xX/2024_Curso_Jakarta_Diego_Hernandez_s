84649721294adcfb512cdf8edce821c322d6e9b4
diegohernandezsamano <samanodiego888@gmail.com>
2024-08-06 11:48:49 -0600

    Inicio del repositorio del curso jakarta

---
+++ jakartadhs/src/main/java/com/samano/itess/jakartadhs/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.samano.itess.jakartadhs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
